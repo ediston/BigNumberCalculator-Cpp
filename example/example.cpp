32925a0fcff442b65840b04a4542c47949124804
ediston <vivek4dhiman@gmail.com>
2015-02-11 14:32:51 +0100

    Intial Commit

---
+++ example/example.cpp
@@ -0,0 +1,11 @@
+#include "../header/BigNumberCalculator.h"
+
+int main() {
+    //Add
+    cout << add("23535345345345353543543454353345", "399353453453455345355435345") << endl;
+    // Multiply
+    cout << multiply("988", "3674") << endl;
+    //103!
+    cout << fact(103) << endl;
+    return 0;
+}
